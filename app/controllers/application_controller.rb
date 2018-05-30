6eaaf1975599d3e6b4413ae80b83bd332abf34e8
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-05-30 19:26:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,18 +17,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-      @post = Post.find_by_id(params[:id])
+      @post = Post.find_by_id(params[:id].to_i)
       binding.pry
       erb :show
     end
 
   get '/posts/:id/edit' do
-    @post = Post.find_by_id(params[:id])
+    @post = Post.find_by_id(params[:id].to_i)
     erb :edit
   end
 
   patch '/posts/:id' do
-    @post = Post.find_by_id(params[:id])
+    @post = Post.find_by_id(params[:id].to_i)
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
